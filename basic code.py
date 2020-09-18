58369e4e0de0cca8a497dad036d173ad910300e2
Yatharth Saroha <yatharthsaroha2003@gmail.com>
2020-09-18 20:01:33 +0530

    Update basic code.py

--- basic code.py
+++ basic code.py
@@ -15,72 +15,11 @@ def insert():
         label = Label(tk, text="fuck yeah 9")
         label.grid(column = 1)
 
-def n0():
-        global string
-        string = string + "0"
-        g = e.get() + "0"
-        e.delete(0, 1000)
-        e.insert(0, g)
 
-def n1():
+def n(number):
         global string
-        string = string + "1"
-        g = e.get() + "1"
-        e.delete(0, 1000)
-        e.insert(0, g)
-
-def n2():
-        global string
-        string = string + "2"
-        g = e.get() + "2"
-        e.delete(0, 1000)
-        e.insert(0, g)
-
-def n3():
-        global string
-        string = string + "3"
-        g = e.get() + "3"
-        e.delete(0, 1000)
-        e.insert(0, g)
-
-def n4():
-        global string
-        string = string + "4"
-        g = e.get() + "4"
-        e.delete(0, 1000)
-        e.insert(0, g)
-
-def n5():
-        global string
-        string = string + "5"
-        g = e.get() + "5"
-        e.delete(0, 1000)
-        e.insert(0, g)
-
-def n6():
-        global string
-        string = string + "6"
-        g = e.get() + "6"
-        e.delete(0, 1000)
-        e.insert(0, g)
-def n7():
-        global string
-        string = string + "7"
-        g = e.get() + "7"
-        e.delete(0, 1000)
-        e.insert(0, g)
-
-def n8():
-        global string
-        string = string + "8"
-        g = e.get() + "8"
-        e.delete(0, 1000)
-        e.insert(0, g)
-
-def n9():
-        global string
-        string = string + "9"
-        g = e.get() + "9"
+        string = string + str(number)
+        g = e.get() + str(number)
         e.delete(0, 1000)
         e.insert(0, g)
 
@@ -142,6 +81,8 @@ def ncbracket():
         e.insert(0, g)
 
 def nclear():
+        global string
+        string = ""
         e.delete(0, 100000)
         
 
@@ -161,16 +102,16 @@ e.grid(row = 1, column = 0, columnspan = 4)
 
 
 
-Button(tk, text="0", command=n0, padx=50, pady=50).grid(row = 6, column = 1)
-Button(tk, text="1", command=n1, padx=50, pady=50).grid(row = 5, column = 0)
-Button(tk, text="2", command=n2, padx=50, pady=50).grid(row = 5, column = 1)
-Button(tk, text="3", command=n3, padx=50, pady=50).grid(row = 5, column = 2)
-Button(tk, text="4", command=n4, padx=50, pady=50).grid(row = 4, column = 0)
-Button(tk, text="5", command=n5, padx=50, pady=50).grid(row = 4, column = 1)
-Button(tk, text="6", command=n6, padx=50, pady=50).grid(row = 4, column = 2)
-Button(tk, text="7", command=n7, padx=50, pady=50).grid(row = 3, column = 0)
-Button(tk, text="8", command=n8, padx=50, pady=50).grid(row = 3, column = 1)
-Button(tk, text="9", command=n9, padx=50, pady=50).grid(row = 3, column = 2)
+Button(tk, text="0", command=lambda: n(0), padx=50, pady=50).grid(row = 6, column = 1)
+Button(tk, text="1", command=lambda: n(1), padx=50, pady=50).grid(row = 5, column = 0)
+Button(tk, text="2", command=lambda: n(2), padx=50, pady=50).grid(row = 5, column = 1)
+Button(tk, text="3", command=lambda: n(3), padx=50, pady=50).grid(row = 5, column = 2)
+Button(tk, text="4", command=lambda: n(4), padx=50, pady=50).grid(row = 4, column = 0)
+Button(tk, text="5", command=lambda: n(5), padx=50, pady=50).grid(row = 4, column = 1)
+Button(tk, text="6", command=lambda: n(6), padx=50, pady=50).grid(row = 4, column = 2)
+Button(tk, text="7", command=lambda: n(7), padx=50, pady=50).grid(row = 3, column = 0)
+Button(tk, text="8", command=lambda: n(8), padx=50, pady=50).grid(row = 3, column = 1)
+Button(tk, text="9", command=lambda: n(9), padx=50, pady=50).grid(row = 3, column = 2)
 Button(tk, text="+", command=nadd, padx=50, pady=50, fg="#3e944a", activeforeground="#3e944a").grid(row = 5, column = 3)
 Button(tk, text="-", command=nsubtract, padx=53, pady=50, fg="#3e944a", activeforeground="#3e944a").grid(row = 4, column = 3)
 Button(tk, text="x", command=nmultiply, padx=52, pady=50, fg="#3e944a", activeforeground="#3e944a").grid(row = 3, column = 3)
@@ -185,5 +126,3 @@ Button(tk, text=")", command=ncbracket, padx=52, pady=50, fg="#3e944a", activefo
 
 
 tk.mainloop()
-
-
